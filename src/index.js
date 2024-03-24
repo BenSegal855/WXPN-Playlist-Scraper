043f8af8eecbb884bddd54e30a2eaa485f6458e6
BenSegal855 <ben@segals.org>
2024-03-24 19:20:32 -0400

    feat: transition to Mongo & calc duration closes #1 Transition to Mongo closes #2 Add duration

--- src/index.js
+++ src/index.js
@@ -1,60 +1,80 @@
+import { setup } from '@skyra/env-utilities';
 import axios from 'axios';
 import moment from 'moment';
-import { createWriteStream } from 'fs';
+import { MongoClient } from 'mongodb';
 import { env } from 'process';
 import { titleCase } from 'title-case';
 
-const pullDate = new Date(env.Start ?? Date.now());
+setup();
+
+const pullDate = moment(new Date(env.Start ?? Date.now()));
 const DAYS_TO_PULL = parseInt(env.Days ?? '1');
+const DIRECTION = env.Direction ?? 'forward';
 console.log('Pull Date', pullDate);
 console.log('Days', DAYS_TO_PULL);
+console.log('Direction', DIRECTION)
 
-const NO_IMAGE = 'https://upload.wikimedia.org/wikipedia/commons/d/dc/No_Preview_image_2.png'
-const FILE_NAME = `WXPN_${moment(pullDate).format('YYYY-MM-DD')}_${DAYS_TO_PULL}_days.csv`;
-const FILE_SEP = '\t'
-
-const data = [];
-
-const file = createWriteStream(FILE_NAME);
-const writeDone = new Promise(resolve => file.once('close', resolve));
-
-file.write(['artist', 'song', 'album', 'airDate', 'image', 'streamPreview'].join(FILE_SEP) + '\n');
-
-console.log('here we go...')
-
-for(let i = DAYS_TO_PULL; i > 0; i--) {
-	console.log(`Getting ${pullDate.toLocaleDateString()}`)
-
-	try {
-		const { data: songs } = await axios.get(`https://origin.xpn.org/utils/playlist/json/${moment(pullDate).format('YYYY-MM-DD')}.json`);
-		data.push(...songs);
-
-		songs.forEach((song, idx) => {
-			const timeslice = song.timeslice ?? song.air_date;
-			if (timeslice && timeslice === (songs[idx + 1]?.timeslice ?? songs[idx + 1]?.air_date)) return;
-			file.write([
-				cleaner(song.artist),
-				cleaner(song.song),
-				cleaner(song.album),
-				timeslice,
-				song.image === '' ?  NO_IMAGE : song.image ?? NO_IMAGE,
-				song.streamPreview
-			].join(FILE_SEP) + '\n')
-		});
-	} catch (e) {
-		console.error(`Unable to get ${pullDate.toLocaleDateString()}, Skipping...`);
-		console.error(e)
-	}
-	pullDate.setDate(pullDate.getDate() + 1);
+if (DIRECTION === 'back') {
+	pullDate.add(DAYS_TO_PULL * -1, 'd');
 }
 
-file.end();
+const NO_IMAGE = 'https://upload.wikimedia.org/wikipedia/commons/d/dc/No_Preview_image_2.png'
+let recordNum = 0
+
+console.log('Connecting to Mongo...');
+const mongo = new MongoClient(env.Mongo);
+const mongoConnect = mongo.connect().then(() => console.log('Mongo Connected!'));
+
+console.log('here we go...');
+
+const dbOps = [];
+const allSongs = [];
+
+for(let i = DAYS_TO_PULL; i > 0; i--) {
+	console.log(`Getting ${pullDate.format('MMM DD YYYY')}`)
+
+	try {
+		const { data: songs } = await axios.get(`https://origin.xpn.org/utils/playlist/json/${pullDate.format('YYYY-MM-DD')}.json`);
+		recordNum += songs.length;
+		songs.reverse();
+		allSongs.push(...songs);
+	} catch (e) {
+		console.error(`Unable to get ${pullDate.format('MMM DD YYYY')}, Skipping...`);
+		console.error(e)
+	}
+	pullDate.add(1, 'd');
+}
+
+console.log(`Getting ${pullDate.format('MMM DD YYYY')} for Duration calc`)
+const lastSong = await axios.get(`https://origin.xpn.org/utils/playlist/json/${moment(pullDate).format('YYYY-MM-DD')}.json`)
+	.then(({ data }) => data[0]);
+allSongs.push(lastSong)
+
+await mongoConnect;
+const db = mongo.db(env.DB).collection('Spins');
+
+console.log('Beginning database write!');
+for (let idx = 0; idx < recordNum; idx++){
+	const currAirDate = allSongs[idx].timeslice ?? allSongs[idx].air_date;
+	const nextAirDate = allSongs[idx + 1].timeslice ?? allSongs[idx + 1].air_date;
+	if (currAirDate && currAirDate === (allSongs[idx + 1]?.timeslice ?? allSongs[idx + 1]?.air_date)) continue;
+
+	dbOps.push(db.findOneAndReplace({ airDate: currAirDate }, {
+		airDate: currAirDate,
+		artist: cleaner(allSongs[idx].artist),
+		title: cleaner(allSongs[idx].song),
+		album: cleaner(allSongs[idx].album),
+		estimatedDuration: Math.round((new Date(nextAirDate).getTime() - new Date(currAirDate).getTime()) / 1000),
+		image: allSongs[idx].image === '' ?  NO_IMAGE : allSongs[idx].image ?? NO_IMAGE,
+		streamPreview: allSongs[idx].streamPreview
+	}, { upsert: true }));
+}
 
 console.log('Loop exited')
-await writeDone;
-
-console.log(`Wrote ${data.length} records`)
+await Promise.all(dbOps)
+console.log(`Wrote ${recordNum} records`)
 console.log('Done!');
+process.exit(0);
 
 /**
  * 
